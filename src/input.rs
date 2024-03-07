d9bf7bf17ca1ea36b53351b00f7c96bd1331fdb1
ktmtrtr <mtkt06311@gmail.com>
2024-03-07 13:28:29 +0900

    Add test: test_get_lines, test_parse_all

--- src/input.rs
+++ src/input.rs
@@ -5,45 +5,49 @@ use cargo_snippet::snippet;
 #[snippet("read")]
 fn read() -> String {
     let mut buf = String::new();
-    std::io::stdin()
-        .read_line(&mut buf)
-        .expect("failed to read line.");
-    buf
+    std::io::stdin().read_line(&mut buf).ok();
+    buf.trim().to_string()
 }
 
-#[snippet(include = "read")]
-#[snippet("get_string")]
-fn get_string(s: String) -> String {
-    s.trim().to_string()
+#[snippet("read_all")]
+fn read_all() -> String {
+    use std::io::Read;
+    let mut buf = Vec::new();
+    std::io::stdin().read_to_end(&mut buf).ok();
+    String::from_utf8(buf).unwrap()
 }
 
-#[snippet(include = "read")]
-#[snippet(include = "get_string")]
-#[snippet("string_to_vec")]
-fn string_to_vec<T: std::str::FromStr>(s: String) -> Vec<T>
+#[snippet("parse_all")]
+fn parse_all<T: std::str::FromStr>(s: String) -> Vec<T>
 where
     <T as std::str::FromStr>::Err: std::fmt::Debug,
 {
-    get_string(s)
-        .split_whitespace()
+    s.split_whitespace()
         .map(|x| x.parse().expect("failed to parse."))
         .collect::<Vec<T>>()
+}
+
+#[snippet("get_lines")]
+fn get_lines(s: String) -> Vec<String> {
+    s.lines().map(|x| x.to_string()).collect::<Vec<String>>()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
     #[test]
-    fn test_get_string() {
-        let input = String::from("Foo\n");
-        assert_eq!(get_string(input), String::from("Foo"));
+    fn test_parse_all() {
+        let input = String::from("0 1 2 3 4");
+        assert_eq!(parse_all::<u64>(input), vec![0, 1, 2, 3, 4]);
+        let input2 = String::from("-1 -2 -3 -4 -5");
+        assert_eq!(parse_all::<i32>(input2), vec![-1, -2, -3, -4, -5]);
     }
 
     #[test]
-    fn test_string_to_vec() {
-        let input = String::from("0 1 2 3 4");
-        assert_eq!(string_to_vec::<u64>(input), vec![0, 1, 2, 3, 4]);
-        let input2 = String::from("-1 -2 -3 -4 -5");
-        assert_eq!(string_to_vec::<i32>(input2), vec![-1, -2, -3, -4, -5]);
+    fn test_get_lines() {
+        let input = String::from("Foo Bar\nFooBar\n");
+        let res = vec!["Foo Bar".to_string(), "FooBar".to_string()];
+        assert_eq!(get_lines(input), res);
     }
 }
