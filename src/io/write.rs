a270b4f533c1e0d26998b4f035b1edbfa0c4ac6f
ktmtrtr <mtkt06311@gmail.com>
2024-04-08 21:32:22 +0900

    Add bufwrite, bufwriteln macros

--- src/io/write.rs
+++ src/io/write.rs
@@ -1,26 +1,26 @@
 #[macro_export]
 macro_rules! bufwrite {
-    ($($arg:tt)*) => {{
-        use ::std::io::{BufWriter, Write};
-        let mut out = BufWriter::new(::std::io::stdout().lock());
-        ::std::write!(out, $($arg)*).unwrap();
-    }};
-}
+            ($($arg:tt)*) => {{
+                use ::std::io::{BufWriter, Write};
+                let mut out = BufWriter::new(::std::io::stdout().lock());
+                ::std::write!(out, $($arg)*).unwrap();
+            }};
+        }
 
 #[macro_export]
 macro_rules! bufwriteln {
-    ($($arg:tt)*) => {{
-            use ::std::io::{BufWriter, Write};
-            let mut out = BufWriter::new(::std::io::stdout().lock());
-            ::std::writeln!(out, $($arg)*).unwrap();
-        }};
-}
+            ($($arg:tt)*) => {{
+                use ::std::io::{BufWriter, Write};
+                let mut out = BufWriter::new(::std::io::stdout().lock());
+                ::std::writeln!(out, $($arg)*).unwrap();
+            }};
+        }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_write() {
-        bufwrite!("{}\n", 1);
-        bufwriteln!("{}", 2);
+        bufwrite!("{} {} {}\n", 1, 2, 3);
+        bufwriteln!("{:.10}", std::f64::consts::E);
     }
 }
