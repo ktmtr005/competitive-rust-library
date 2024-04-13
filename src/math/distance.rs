41c69bd669b5a17825fddc5e3ea7f5c0ce2dd97f
ktmtrtr <mtkt06311@gmail.com>
2024-04-13 13:28:09 +0900

    feat: dist2 for f32, f64

--- src/math/distance.rs
+++ src/math/distance.rs
@@ -6,9 +6,23 @@ where
     T: std::ops::Add<Output = T>,
     T: std::ops::Sub<Output = T>,
     T: std::ops::Mul<Output = T>,
+    T: PartialOrd,
 {
-    let diff_x = x1 - x2;
-    let diff_y = y1 - y2;
+    use std::cmp::Ordering;
+    let (x_greater, x_less) =
+        if x1.partial_cmp(&x2).expect("Comparison with NaN") == Ordering::Greater {
+            (x1, x2)
+        } else {
+            (x2, x1)
+        };
+    let (y_greater, y_less) =
+        if y1.partial_cmp(&y2).expect("Comparison with NaN") == Ordering::Greater {
+            (y1, y2)
+        } else {
+            (y2, y1)
+        };
+    let diff_x = x_greater - x_less;
+    let diff_y = y_greater - y_less;
     diff_x * diff_x + diff_y * diff_y
 }
 
@@ -18,5 +32,8 @@ mod tests {
     #[test]
     fn test_dist2() {
         assert_eq!(dist2::<i32>(&(0, 0), &(3, 4)), 25);
+        assert_eq!(dist2::<usize>(&(0, 0), &(1, 2)), 5);
+        assert_eq!(dist2::<u32>(&(0, 0), &(0, 0)), 0);
+        assert_eq!(dist2::<f64>(&(0.0, 0.0), &(3.0, 4.0)), 25.0);
     }
 }
